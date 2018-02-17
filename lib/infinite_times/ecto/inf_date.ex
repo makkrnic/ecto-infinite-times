d2aa1a8d626a8d0156075f79cccdad19f8cec448
Mak Krnic <mak@makkrnic.com>
2018-02-17 12:37:10 +0100

    code formatting and @impl
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

--- lib/infinite_times/ecto/inf_date.ex
+++ lib/infinite_times/ecto/inf_date.ex
@@ -4,40 +4,53 @@ defmodule InfiniteTimes.Ecto.InfDate do
   """
   @behaviour Ecto.Type
 
+  @impl Ecto.Type
   def type, do: :date
 
+  @impl Ecto.Type
   def cast(finitness) when finitness in [:infinity, :neg_infinity],
     do: {:ok, InfiniteTimes.InfDate.new(finitness)}
-  def cast(%InfiniteTimes.InfDate{} = d),
-    do: {:ok, d}
-  def cast(%Date{} = d),
-    do: {:ok, d |> InfiniteTimes.InfDate.new()}
+
+  def cast(%InfiniteTimes.InfDate{} = d), do: {:ok, d}
+  def cast(%Date{} = d), do: {:ok, d |> InfiniteTimes.InfDate.new()}
+
   def cast(d) when is_binary(d) do
     with {:ok, date} <- Ecto.Date.cast(d),
-      {:ok, date} <- Date.new(date.year, date.month, date.day) do
-        {:ok, date |> InfiniteTimes.InfDate.new}
-      else
-        _ -> :error
+         {:ok, date} <- Date.new(date.year, date.month, date.day) do
+      {:ok, date |> InfiniteTimes.InfDate.new()}
+    else
+      _ -> :error
     end
   end
+
   def cast(_), do: :error
 
+  @impl Ecto.Type
   def load(finitness) when finitness in [:infinity, :neg_infinity] do
     {:ok, InfiniteTimes.InfDate.new(finitness)}
   end
+
   def load({{year, month, day}, :finite}) do
     case Date.new(year, month, day) do
       {:ok, date} -> {:ok, date |> InfiniteTimes.InfDate.new()}
       _ -> :error
     end
   end
+
   def load(_), do: :error
 
-  def dump(%InfiniteTimes.InfDate{finitness: finitness}) when finitness in [:infinity, :neg_infinity] do
+  @impl Ecto.Type
+  def dump(%InfiniteTimes.InfDate{finitness: finitness})
+      when finitness in [:infinity, :neg_infinity] do
     {:ok, finitness}
   end
-  def dump(%InfiniteTimes.InfDate{date: %Date{year: year, month: month, day: day}, finitness: :finite}) do
+
+  def dump(%InfiniteTimes.InfDate{
+        date: %Date{year: year, month: month, day: day},
+        finitness: :finite
+      }) do
     {:ok, {year, month, day}}
   end
+
   def dump(_), do: :error
 end
--- lib/infinite_times/inf_date.ex
+++ lib/infinite_times/inf_date.ex
@@ -5,7 +5,9 @@ defmodule InfiniteTimes.InfDate do
 
   defstruct [:date, :finitness]
 
-  @type t :: %__MODULE__{date: %Date{}, finitness: :finite} | %__MODULE__{date: nil, finitness: :infinity | :neg_infinity}
+  @type t ::
+          %__MODULE__{date: %Date{}, finitness: :finite}
+          | %__MODULE__{date: nil, finitness: :infinity | :neg_infinity}
 
   @spec new(atom()) :: t
   def new(:infinity), do: %__MODULE__{date: nil, finitness: :infinity}
@@ -13,27 +15,28 @@ defmodule InfiniteTimes.InfDate do
   def new(%Date{} = date), do: %__MODULE__{date: date, finitness: :finite}
 
   def from_erl(erl_date) do
-    case erl_date |> Date.from_erl do
+    case erl_date |> Date.from_erl() do
       {:ok, date} -> {:ok, date |> new()}
       x -> x
     end
   end
 
   @spec compare(t, t) :: :lt | :eq | :gt
-  def compare(%__MODULE__{finitness: :infinity}, %__MODULE__{finitness: :infinity}),
-    do: :eq
-  def compare(%__MODULE__{finitness: :infinity}, _),
-    do: :gt
-  def compare(_, %__MODULE__{finitness: :infinity}),
-    do: :lt
+  def compare(%__MODULE__{finitness: :infinity}, %__MODULE__{finitness: :infinity}), do: :eq
+  def compare(%__MODULE__{finitness: :infinity}, _), do: :gt
+  def compare(_, %__MODULE__{finitness: :infinity}), do: :lt
+
   def compare(%__MODULE__{finitness: :neg_infinity}, %__MODULE__{finitness: :neg_infinity}),
     do: :eq
-  def compare(%__MODULE__{finitness: :neg_infinity}, _),
-    do: :lt
-  def compare(_, %__MODULE__{finitness: :neg_infinity}),
-    do: :gt
-  def compare(%__MODULE__{date: left, finitness: :finite}, %__MODULE__{date: right, finitness: :finite}),
-    do: Date.compare left, right
+
+  def compare(%__MODULE__{finitness: :neg_infinity}, _), do: :lt
+  def compare(_, %__MODULE__{finitness: :neg_infinity}), do: :gt
+
+  def compare(%__MODULE__{date: left, finitness: :finite}, %__MODULE__{
+        date: right,
+        finitness: :finite
+      }),
+      do: Date.compare(left, right)
 
   def is?(:gte, left, right), do: compare(left, right) in [:eq, :gt]
   def is?(:lte, left, right), do: compare(left, right) in [:eq, :lt]
